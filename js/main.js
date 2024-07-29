880d6b868bf3a1a46df046107cc0c8fb41718b62
F1B1 <F1B1>
2024-07-29 18:54:20 +0300

    Updates

--- js/main.js
+++ js/main.js
@@ -5486,7 +5486,7 @@ function animateHeader() {
     opacity: 0
   }, {
     opacity: 1
-  }, 2);
+  }, 1);
 }
 function animateColumns(columnsClass, columnClass) {
   const columns = document.querySelector(columnsClass);
@@ -6324,99 +6324,6 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony export */ });
 /* harmony import */ var just_validate__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! just-validate */ "./node_modules/just-validate/dist/just-validate.es.js");
 /* harmony import */ var _node_modules_inputmask_dist_inputmask_es6_js__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ../../../node_modules/inputmask/dist/inputmask.es6.js */ "./node_modules/inputmask/dist/inputmask.es6.js");
-// import JustValidate from 'just-validate';
-// import Inputmask from "../../../node_modules/inputmask/dist/inputmask.es6.js";
-// import intlTelInput from 'intl-tel-input';
-
-// export const validateForms = (selector, rules, checkboxes = [], afterSend) => {
-
-//   const form = document?.querySelector(selector);
-//   const telSelector = form?.querySelector('input[type="tel"]');
-
-//   if (!form) {
-//     console.error('Нет такого селектора!');
-//     return false;
-//   }
-
-//   if (!rules) {
-//     console.error('Вы не передали правила валидации!');
-//     return false;
-//   }
-
-//   if (telSelector) {
-//     const iti = intlTelInput(telSelector, {
-//       initialCountry: 'auto',
-//       geoIpLookup: function(callback) {
-//         fetch('https://ipinfo.io/json')
-//           .then(response => response.json())
-//           .then(data => {
-//             callback(data.country);
-//           })
-//           .catch(() => {
-//             callback('us');
-//           });
-//       },
-//       utilsScript: 'https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/17.0.8/js/utils.js'
-//     });
-
-//     telSelector.addEventListener('countrychange', function() {
-//       Inputmask.remove(telSelector);
-//       Inputmask(iti.getSelectedCountryData().dialCode + " (999) 999-9999").mask(telSelector);
-//     });
-
-//     for (let item of rules) {
-//       if (item.tel) {
-//         item.rules.push({
-//           rule: 'function',
-//           validator: function() {
-//             const phone = telSelector.value.replace(/[^0-9]/g, '');
-//             return iti.isValidNumber();
-//           },
-//           errorMessage: item.telError
-//         });
-//       }
-//     }
-//   }
-
-//   const validation = new JustValidate(selector);
-
-//   for (let item of rules) {
-//     validation.addField(item.ruleSelector, item.rules);
-//   }
-
-//   if (checkboxes.length) {
-//     for (let item of checkboxes) {
-//       validation.addRequiredGroup(
-//         `${item.selector}`,
-//         `${item.errorMessage}`
-//       )
-//     }
-//   }
-
-//   validation.onSuccess((ev) => {
-//     let formData = new FormData(ev.target);
-
-//     let xhr = new XMLHttpRequest();
-
-//     xhr.onreadystatechange = function () {
-//       if (xhr.readyState === 4) {
-//         if (xhr.status === 200) {
-//           if (afterSend) {
-//             afterSend();
-//           }
-//           console.log('Отправлено');
-//         }
-//       }
-//     }
-
-//     xhr.open('POST', 'mail.php', true);
-//     xhr.send(formData);
-
-//     ev.target.reset();
-//   });
-
-// };
-
 
 
 const validateForms = function (selector, rules) {
